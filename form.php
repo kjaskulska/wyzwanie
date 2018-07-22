c3c621f77cb28bcc6e4cc3b8347a1f9f67866232
kjaskulska <k.u.pietrzak@gmail.com>
2018-07-22 14:43:36 +0200

    ostylowanie formularza

--- form.php
+++ form.php
@@ -9,14 +9,3 @@
         mail($to, $subject, $message, $headers);
         echo '<h2>Dzięki wielkie za wysłanie wiadomości :)</h2>';
     ?>
-
-    <form action="form.php" method="POST">
-    <label for="name">Imię</label>
-      <input type="text" name="name" id="name">
-    <label for="email">E-mail</label>
-      <input type="email" name="email" id="email">
-    <label for="massage">Tekst wiadomości</label>
-      <textarea name="massage" id="massage" rows="3" required/></textarea>
-      <input type="submit" name="submit" value="Wyślij wiadomość">
-      <input type="reset" name="submit" value="Reset danych">
-  </form>
